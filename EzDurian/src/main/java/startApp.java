e27d26eefc615e8899244492ce0ecb0efed7ad9e
Hasan Suhaimi <srip_arif@yahoo.com.my>
2020-11-28 15:40:27 +0800

    Update startApp.java

--- EzDurian/src/main/java/startApp.java
+++ EzDurian/src/main/java/startApp.java
@@ -246,7 +246,8 @@ public class startApp {
                                 ",%0A <b>Confirmed at :</b> "+ confirmedAt +
                                 ",%0A <b>Recipient:</b> NP. "+globalIndex + " "+ name +
                                 ",%0A <b>Product :</b> "+ product +
-                                ",%0A <b>Alternative no :</b> "+ field2 +
+                                ",%0A <b>Alternative no :</b> "+ field2+
+                                ",\n <b>Primary no :</b> "+ ContNumber  +
                                 ",%0A <b>Delivery info :</b> "+ field1 +
                                 ",%0A <b>Recipient address :</b> "+ address );
 
